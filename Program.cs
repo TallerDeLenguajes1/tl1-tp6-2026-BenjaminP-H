b761b94a4d8dc25d33850b721a5027f9eedc9089
Benjamin <benjaminpereina@gmail.com>
2026-05-16 13:21:42 -0300

    tutorial basico de instalacion y uso de C++

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int a;
+
+int b;
+
+a=20;
+
+b=a;
+
+Console.WriteLine("Valor de a:"+a);
+
+Console.WriteLine("Valor de b:"+b);
