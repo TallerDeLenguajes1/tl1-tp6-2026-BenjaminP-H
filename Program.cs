30c3adcaef49b017a52a801da6e3b7589da23f94
Benjamin Pereira Herrera <benjaminpereina@gmail.com>
2026-06-03 10:56:46 -0300

    primer punto

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ingrese un numero");//comando para escribir 
+string numeroText = Console.ReadLine();//el console.readline es el printf con scanf y lo guardo en una variable string por lo general 
 
+if(int.TryParse(numeroText, out int numero)) //el tryparse compara si el texto es un numero y lo guardo en una variable entero
+{
+    if(numero > 9)
+    {
+        char[] caracteres = numeroText.ToCharArray();//con el tocharArray Creo los caracteres que ingrese o tengo en una variable y lo guardo en un arreglo
 
-int a;
+        Array.Reverse(caracteres);//el array.reverse invierte el arreglo
 
-int b;
+        string invertido = new string(caracteres); //preparo la estancia con new que tengo entendido es parecido a reservar memoria
 
-a=20;
-
-b=a;
-
-Console.WriteLine("Valor de a:"+a);
-
-Console.WriteLine("Valor de b:"+b);
+        Console.WriteLine("Numero invertidoo: " + invertido);
+    }
+    else
+    {
+        Console.WriteLine("El numero debe ser mayor que 0");
+    }
+}
+else
+{
+    Console.WriteLine("Error, ingrese un numero valido");
+}
